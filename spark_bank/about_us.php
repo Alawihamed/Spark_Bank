1090ef914aa98d47921a220951ce203e4948cd26
Alawi Hamed Alaidaros <42833290+Alawihamed@users.noreply.github.com>
2020-12-16 19:16:42 +0530

    Add files via upload

---
+++ spark_bank/about_us.php
@@ -0,0 +1,29 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="about.css">
+</head>
+
+<body>
+    <div class="about">
+        <div class="about">
+            <h1 id="contact">About Us</h1>
+        </div>
+
+        <div class="flex-container" style="border-bottom: 0; border-top-left-radius: 10px; border-top-right-radius: 10px;">
+        </div>
+				<p style="padding: 30px; align-text:justify; margin-top:50px; font-size:30px;">
+                    Sparks Bank is a service driven bank that promises to be a one-stop solution for its customers' core banking, agency and utility requirements.
+                    Founded in 2018, it is already trusted by over 15 million individuals and organisations across India, who avail our various services through over 2000 branches.
+                    To know more, join our fast-growing family by visiting your nearest Janseva branch, or calling on our toll-free numbers provided 
+                    <a target="_blank" href="contact.php" style="color: #4ba5a5;"> here. </a>
+                </p>
+    </div>
+
+</body>
+</html>
